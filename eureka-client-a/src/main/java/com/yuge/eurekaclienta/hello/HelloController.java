3572f7f5f1bc5950b25be3619705611c24e5d884
yuge <yuge1805@yeah.net>
2019-10-18 11:39:16 +0800

    eureka client初始化

---
+++ eureka-client-a/src/main/java/com/yuge/eurekaclienta/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.yuge.eurekaclienta.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello eureka";
+    }
+
+}
